aae919f72580310d1107f7d190b1352e87d97f41
JoshUndefined <JoshUndefined.tv@gmail.com>
2025-08-26 12:23:29 -0700

    Loop through loadout export file

--- src/main.py
+++ src/main.py
@@ -25,90 +25,77 @@ def main():
     logger.info("DIM Loadout Archiver")
     logger.debug("Verbose logging enabled")
 
+
     # TODO: Download the manifest locally instead
     manifest = D2Manifest(Path.cwd() / "data/manifest.db", verbose=True)
 
-    # https://github.com/DestinyItemManager/dim-api/blob/master/api/shapes/loadouts.ts
-    logger.info("Example DIM Loadout")
-    logger.info("'S25 Solo GM Birthplace Healing'")
-    logger.info("\n\n\n-------------------------------------------\n-----equipped:\n\n\n")
-    logger.info(manifest.get_inventory_item(2357297366)) # Witherhoard Exotic Primary
-    logger.info(manifest.get_inventory_item(859869931)) # Nullify Legendary Energy
-    logger.info(manifest.get_inventory_item(1762785662)) # VS Chill Inhibitor Legendary Power
-    logger.info(manifest.get_inventory_item(50291571)) # Speaker's Sight
-    logger.info(manifest.get_inventory_item(119228495))
-    logger.info(manifest.get_inventory_item(1011882337))
-    logger.info(manifest.get_inventory_item(3663544278))
-    logger.info(manifest.get_inventory_item(3685276035))
-    logger.info(manifest.get_inventory_item(3941205951)) # Dawnblade Subclass
-    logger.info("\n\n\n-------------------------------------------\n-----item 3941205951 socket overrides:\n\n\n")
-    logger.info(manifest.get_inventory_item(2979486801))
-    logger.info(manifest.get_inventory_item(3686638442))
-    logger.info(manifest.get_inventory_item(2274196884))
-    logger.info(manifest.get_inventory_item(1470370538))
-    logger.info(manifest.get_inventory_item(1841016428))
-    logger.info(manifest.get_inventory_item(83039192))
-    logger.info(manifest.get_inventory_item(83039193))
-    logger.info(manifest.get_inventory_item(362132294))
-    logger.info(manifest.get_inventory_item(362132300))
-    logger.info(manifest.get_inventory_item(362132289))
-    logger.info(manifest.get_inventory_item(362132293))
-    logger.info("\n\n\n-------------------------------------------\n-----unequipped:\n\n\n")
-    # logger.info("-----mods:")
-    # logger.info(manifest.get_inventory_item(1180408010))
-    # logger.info(manifest.get_inventory_item(3832366019))
-    # logger.info(manifest.get_inventory_item(3832366019))
-    # logger.info(manifest.get_inventory_item(554409585))
-    # logger.info(manifest.get_inventory_item(539459624))
-    # logger.info(manifest.get_inventory_item(1180408010))
-    # logger.info(manifest.get_inventory_item(1079896271))
-    # logger.info(manifest.get_inventory_item(377010989))
-    # logger.info(manifest.get_inventory_item(4287799666))
-    # logger.info(manifest.get_inventory_item(3410844187))
-    # logger.info(manifest.get_inventory_item(3410844187))
-    # logger.info(manifest.get_inventory_item(3719981603))
-    # logger.info(manifest.get_inventory_item(1180408010))
-    # logger.info(manifest.get_inventory_item(4087056174))
-    # logger.info(manifest.get_inventory_item(335129856))
-    # logger.info(manifest.get_inventory_item(4087056174))
-    # logger.info(manifest.get_inventory_item(1180408010))
-    # logger.info(manifest.get_inventory_item(4188291233))
-    # logger.info(manifest.get_inventory_item(11126525))
-    # logger.info(manifest.get_inventory_item(40751621))
-    # logger.info(manifest.get_inventory_item(617569843))
-    logger.info("\n\n\n-------------------------------------------\n-----mods by bucket:\n\n\n")
-    logger.info(f"-- {format(manifest.get_inventory_bucket(14239492))}")
-    logger.info(manifest.get_inventory_item(965288073))
-    logger.info(manifest.get_inventory_item(3975122240))
-    logger.info(f"-- {format(manifest.get_inventory_bucket(20886954))}")
-    logger.info(manifest.get_inventory_item(965288073))
-    logger.info(manifest.get_inventory_item(2596599281))
-    logger.info(f"-- {format(manifest.get_inventory_bucket(1585787867))}")
-    logger.info(manifest.get_inventory_item(965288073))
-    logger.info(manifest.get_inventory_item(3303607908))
-    logger.info(f"-- {format(manifest.get_inventory_bucket(3448274439))}")
-    logger.info(manifest.get_inventory_item(965288073))
-    logger.info(manifest.get_inventory_item(702981643))
-    logger.info(f"-- {format(manifest.get_inventory_bucket(3551918588))}")
-    logger.info(manifest.get_inventory_item(965288073))
-    logger.info(manifest.get_inventory_item(2179078680))
-    logger.info("\n\n\n-------------------------------------------\n-----inGameIdentifiers:\n\n\n")
-    logger.info("https://bungie.net/" + manifest.get_loadout_color(3871954967).get("colorImagePath"))
-    logger.info("https://bungie.net/" + manifest.get_loadout_icon(797343698).get("iconImagePath"))
-    logger.info(manifest.get_loadout_name(2755629635).get("name"))
-    logger.info("\n\n\n-------------------------------------------\n-----artifactUnlocks:\n\n\n")
-    logger.info(manifest.get_inventory_item(4152932100))
-    logger.info(manifest.get_inventory_item(4152932103))
-    logger.info(manifest.get_inventory_item(4152932097))
-    logger.info(manifest.get_inventory_item(2317325398))
-    logger.info(manifest.get_inventory_item(2317325399))
-    logger.info(manifest.get_inventory_item(2317325393))
-    logger.info(manifest.get_inventory_item(3547711350))
-    logger.info(manifest.get_inventory_item(3547711348))
-    logger.info(manifest.get_inventory_item(3547711346))
-    logger.info(manifest.get_inventory_item(51032917))
-    logger.info(manifest.get_inventory_item(1328115226))
-    logger.info(manifest.get_inventory_item(1328115229))
+
+    with open("data/dim-api_loadouts_20250711-0854.json") as f: # DEBUG: Hard coded file name for now
+        dim_loadouts = json.load(f)
+        # logger.info(dim_loadouts)
+    
+    # print(dim_loadouts["sync"])
+    # print(dim_loadouts["loadouts"])
+    # print(dim_loadouts["syncToken"])
+    
+    for loadout in dim_loadouts["loadouts"]:
+        # # DEBUG look at a specific loadout
+        # if loadout["id"] != "015918bc-a732-4c27-b8f2-54d708b67424":
+        #     continue
+            
+        logger.info(f"-------------------------------------------\nLoadout {loadout['id']} {loadout['name']}")
+        # logger.info(loadout) # DEBUG https://github.com/DestinyItemManager/dim-api/blob/master/api/shapes/loadouts.ts
+
+        logger.info("\n-------------------------------------------\n-----info:\n")
+        logger.info(f"DIM id: {loadout['id']}")
+        logger.info(f"name: {loadout['name']}")
+        logger.info(f"classType: {loadout['classType']}")
+        logger.info(f"createdAt: {loadout['createdAt']}")
+        logger.info(f"lastUpdatedAt: {loadout['lastUpdatedAt']}")
+        logger.info(f"notes: {loadout['notes']}")
+        if "inGameIdentifiers" in loadout["parameters"].keys():
+            inGameIdentifier = loadout["parameters"]["inGameIdentifiers"]
+            logger.info("inGameLoadoutColor: https://bungie.net/" + manifest.get_loadout_color(inGameIdentifier["colorHash"]).get("colorImagePath"))
+            logger.info("inGameLoadoutIcon: https://bungie.net/" + manifest.get_loadout_icon(inGameIdentifier["iconHash"]).get("iconImagePath"))
+            logger.info("inGameLoadoutName: " + manifest.get_loadout_name(inGameIdentifier["nameHash"]).get("name"))
+    
+        logger.info("\n-------------------------------------------\n-----equipped:\n")
+        for equipped in loadout["equipped"]:
+            logger.info(manifest.get_inventory_item(equipped["hash"]))
+
+            if "socketOverrides" in equipped.keys():
+            # Subclass selectable details like Super, Aspects, Abilities
+                logger.info("\n-------------------------------------------\n-----socketOverrides:\n")
+                for subclass in equipped["socketOverrides"].values():
+                    logger.info(manifest.get_inventory_item(subclass))
+
+        logger.info("\n-------------------------------------------\n-----unequipped:\n")
+        for unequipped in loadout["unequipped"]:
+            logger.info(manifest.get_inventory_item(unequipped["hash"]))
+
+        logger.info("\n-------------------------------------------\n-----mods:\n")
+        # These are "loose" armor mods set in DIM that the DIM loadout generator will assign to armor
+        for mod in loadout["parameters"]["mods"]:
+            logger.info(manifest.get_inventory_item(mod))
+
+        logger.info("\n-------------------------------------------\n-----mods by bucket:\n")
+        # Cosmetic armor mods by armor slot
+        for inventoryBucket in loadout["parameters"]["modsByBucket"]:
+            logger.info(f"-- Bucket: {manifest.get_inventory_bucket(int(inventoryBucket))}")
+            for mod in loadout["parameters"]["modsByBucket"][inventoryBucket]:
+                # logger.info(mod)
+                logger.info(manifest.get_inventory_item(mod))
+
+        if "artifactUnlocks" in loadout["parameters"].keys():
+            logger.info("\n-------------------------------------------\n-----artifactUnlocks:\n")
+            # Seasonal artifact mods
+            for artifactMod in loadout["parameters"]["artifactUnlocks"]["unlockedItemHashes"]:
+                # logger.info(artifactMod)
+                logger.info(manifest.get_inventory_item(artifactMod))
+
+
+
+        break # DEBUG just running the first found loadout for now
 
 
 if __name__ == "__main__":
